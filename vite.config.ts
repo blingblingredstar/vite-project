3d0f3c214356da3dc4f8bbfccf96f817c5c8abfe
yong liu <564447023@qq.com>
2023-01-17 11:54:24 +0800

    Feat: add support for stylelint

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteEslint from 'vite-plugin-eslint';
+import viteStylelint from 'vite-plugin-stylelint';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteEslint()]
+  plugins: [
+    react(),
+    viteEslint(),
+    viteStylelint({
+      exclude: ['windicss', 'node_modules']
+    })
+  ]
 });
